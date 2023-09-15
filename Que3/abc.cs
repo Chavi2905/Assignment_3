a6f8c703d194cc4270a83dece45b5a988e84426e
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:40:46 +0530

    Add files via upload

---
+++ Que3/abc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que3
+{
+    internal class abc
+    {
+        internal int x=0;
+        internal void method()
+        {
+            x++;
+            x++;
+            Console.WriteLine(x);
+        }
+    }
+}
