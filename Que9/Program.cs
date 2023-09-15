33df6f50e3281e9ab6a051c137560c4ec3f934c0
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:45:49 +0530

    Add files via upload

---
+++ Que9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que9
+{
+    public class Division
+    {
+        protected double DivideNumbers(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+                return double.NaN;
+            }
+            return dividend / divisor;
+        }
+    }
+
+    // Derived class Calculator
+    public class Calculator : Division
+    {
+        public double PerformDivision(double dividend, double divisor)
+        {
+            // Access the protected method from the base class
+            return DivideNumbers(dividend, divisor);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the dividend: ");
+            double dividend = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the divisor: ");
+            double divisor = Convert.ToDouble(Console.ReadLine());
+
+            Calculator calculator = new Calculator();
+
+            double result = calculator.PerformDivision(dividend, divisor);
+
+            Console.WriteLine($"Result of division: {result}");
+            Console.ReadLine();
+        }
+    }
+}
