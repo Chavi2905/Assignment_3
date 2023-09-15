0580c85e3ffcdec69c67863f0059ba37cdcee4bd
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:39:26 +0530

    Add files via upload

---
+++ Que1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que1
+{
+    internal class Program
+    {
+        class MyClass
+        {
+            private int x;
+            private int y;
+           
+
+            public void SetValue(int value1,int value2)
+            {
+                x = value1;
+                y = value2;
+                
+            }
+
+            public int GetValue()
+            {
+                return x+y; 
+            }
+        }
+
+        class main
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter an integer value:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter an integer value:");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                MyClass obj = new MyClass();
+                int sum= obj.GetValue();    
+
+                Console.WriteLine("Value set in the class: " + obj.GetValue());
+                Console.ReadLine();
+            }
+        }
+       
+
+    }
+}
