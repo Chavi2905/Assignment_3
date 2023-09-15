1a09cf58657bbf19c3d95750fe8d0dd5465b48e4
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:43:13 +0530

    Add files via upload

---
+++ Que6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que6
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // Derived class Dog
+    public class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} says woof!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of the animal: ");
+            string animalName = Console.ReadLine();
+
+     
+            Dog myDog = new Dog(animalName);
+
+          
+            Console.WriteLine($"My animal's name is {myDog.Name}");
+            myDog.Bark();
+        }
+    }
+}
