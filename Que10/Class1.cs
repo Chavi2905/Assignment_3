432fba44da9b87d31b58aa2c113af04b9d82fdd8
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:46:28 +0530

    Add files via upload

---
+++ Que10/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que10
+{
+    class ABC
+    {
+        public void Method()
+        {
+            MyClass obj = new MyClass();
+            obj.PublicMethod();
+        }
+    }
+    class main
+    {
+        static void Main(string[] args)
+        {
+            ABC obj1 = new ABC();
+            obj1.Method();
+        }
+    }
+}
---
+++ Que10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Que10
+{
+    
+
+public class MyClass
+    {
+        internal MyClass()
+        {
+            Console.WriteLine("Internal constructor called.");
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method called.");
+        }
+    }
+
+
+
+
+
+}
