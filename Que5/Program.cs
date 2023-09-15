76b621e9bc7c84780b8309e3b1e5062ab547d294
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:42:19 +0530

    Add files via upload

---
+++ Que5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que5
+{
+    // Assembly A
+
+    
+        public class Bank
+        {
+            protected internal double accountBalance;
+
+            public Bank(double initialBalance)
+            {
+                accountBalance = initialBalance;
+            }
+
+        public void DisplayBalance()
+        {
+            
+            
+                Console.WriteLine($"Account Balance: {accountBalance:C}");
+            
+        }
+        }
+    
+}
---
+++ Que5/XYZ.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;{
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que5
+    internal class XYZ
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank(1000.00);
+
+            // Access the protected internal member
+            Console.WriteLine($"Initial Balance: {bank.accountBalance:C}");
+
+            bank.DisplayBalance();
+            Console.ReadLine();
+        }
+    }
+}
