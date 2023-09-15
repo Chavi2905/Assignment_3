7caf99222b1d6a1eb74e8d07db2474d53e5d4b18
Chavi2905 <143154223+Chavi2905@users.noreply.github.com>
2023-09-15 08:45:14 +0530

    Add files via upload

---
+++ QUE8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUE8
+{
+    public class MyClass
+    {
+        private void PrivateMethod()
+        {
+            Console.WriteLine("This is a private method.");
+            Console.ReadLine();
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("This is a public method.");
+            PrivateMethod(); // Call the private method from the public method
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myObject = new MyClass();
+
+            // Call the public method, which in turn calls the private method
+            myObject.PublicMethod();
+        }
+    }
+}
